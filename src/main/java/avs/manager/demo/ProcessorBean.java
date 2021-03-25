b843c7b9fe5b0e84abf59dd0ba9a9a9e11ddcafb
Anant Yash Pande <anantyash9@gmail.com>
2021-03-25 14:43:59 +0530

    changed header

--- src/main/java/avs/manager/demo/ProcessorBean.java
+++ src/main/java/avs/manager/demo/ProcessorBean.java
@@ -3,23 +3,38 @@ package avs.manager.demo;
 import java.util.Random;
 
 import org.apache.camel.Exchange;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import com.google.protobuf.Timestamp;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 @Component("ProcessorBean")
 public class ProcessorBean {
+	private Timestamp timestamp;
+	private int count;
+	private final static Logger LOGGER = 
+            Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
-    private int counter;
 
     public String hashIt(String body) {
 //    	System.out.println(body.hashCode()%3);
         return String.valueOf(Math.abs(body.hashCode()%3));
     }
     public void checkSequence (Exchange exchange) {
-    	System.out.println(exchange.getIn().getHeaders());
+    	Timestamp temp =(Timestamp) exchange.getIn().getHeader("CamelGooglePubsub.PublishTime");
+    	if (this.timestamp ==null) {
+    	this.timestamp = temp;
+    	}
+    	double seconds=temp.getSeconds()+temp.getNanos()*1e-9;
+    	double last_message = timestamp.getSeconds()+timestamp.getNanos()*1e-9;
+    	if (seconds-last_message <0) {
+    		count++;
+    		LOGGER.log(Level.INFO,"Total Messages out of order "+count);
+    		
+    	}
+    		
+    	}
     	
-    	
-    }
 
 }
